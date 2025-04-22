5a19372830bedca4742657c97244f1f47c356c5b
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2025-04-22 05:08:32 -0400

    Add `get_slot0` method
    Introduce `get_slot0` for querying pool Slot0 details like sqrtPriceX96, tick, protocolFee, and lpFee. Add `get_position_info_slot` to compute position-specific state slots. Refactor liquidity calculations for better safety and clarity, and include a test for `get_slot0` functionality.

--- src/extensions/pool_manager_lens.rs
+++ src/extensions/pool_manager_lens.rs
@@ -11,13 +11,20 @@ use alloy::{
     providers::Provider,
     uint,
 };
-use alloy_primitives::{keccak256, Address, B256, U256};
+use alloy_primitives::{
+    aliases::{I24, U24},
+    keccak256, Address, B256, U160, U256,
+};
 use alloy_sol_types::SolValue;
 use uniswap_v3_sdk::prelude::*;
 
 const POOLS_SLOT: U256 = uint!(6_U256);
+const FEE_GROWTH_GLOBAL0_OFFSET: U256 = uint!(1_U256);
+const FEE_GROWTH_GLOBAL1_OFFSET: U256 = uint!(2_U256);
+const LIQUIDITY_OFFSET: U256 = uint!(3_U256);
 const TICKS_OFFSET: U256 = uint!(4_U256);
 const TICK_BITMAP_OFFSET: U256 = uint!(5_U256);
+const POSITIONS_OFFSET: U256 = uint!(6_U256);
 
 fn get_pool_state_slot(pool_id: B256) -> U256 {
     U256::from_be_bytes(keccak256((pool_id, POOLS_SLOT).abi_encode()).0)
@@ -33,6 +40,12 @@ fn get_tick_info_slot<I: TickIndex>(pool_id: B256, tick: I) -> U256 {
     let state_slot = get_pool_state_slot(pool_id);
     let ticks_mapping_slot = state_slot + TICKS_OFFSET;
     U256::from_be_bytes(keccak256((tick.to_i24(), ticks_mapping_slot).abi_encode()).0)
+}
+
+fn get_position_info_slot(pool_id: B256, position_id: B256) -> U256 {
+    let state_slot = get_pool_state_slot(pool_id);
+    let position_mapping_slot = state_slot + POSITIONS_OFFSET;
+    U256::from_be_bytes(keccak256((position_id, position_mapping_slot).abi_encode()).0)
 }
 
 /// A lens for querying Uniswap V4 pool manager
@@ -58,6 +71,50 @@ where
             manager: IExtsload::new(manager, provider),
             _network: core::marker::PhantomData,
         }
+    }
+
+    /// Retrieves the Slot0 of a pool: sqrtPriceX96, tick, protocolFee, lpFee
+    ///
+    /// ## Arguments
+    ///
+    /// * `pool_id`: The ID of the pool
+    /// * `block_id`: Optional block ID to query at
+    ///
+    /// ## Returns
+    ///
+    /// * `sqrtPriceX96`: The square root of the price of the pool, in Q96 precision
+    /// * `tick`: The current tick of the pool
+    /// * `protocol_fee`: The protocol fee of the pool
+    /// * `lp_fee`: The swap fee of the pool
+    #[inline]
+    pub async fn get_slot0(
+        &self,
+        pool_id: B256,
+        block_id: Option<BlockId>,
+    ) -> Result<(U160, I24, U24, U24), Error> {
+        let block_id = block_id.unwrap_or(BlockId::Number(BlockNumberOrTag::Latest));
+        let state_slot = get_pool_state_slot(pool_id);
+        let data = self
+            .manager
+            .extsload_0(B256::from(state_slot))
+            .block(block_id)
+            .call()
+            .await?
+            .value;
+
+        let sqrt_price_x96 = U160::from_be_slice(&data[12..32]);
+
+        let tick_bytes = unsafe { (data.as_ptr().add(9) as *const [u8; 3]).read_unaligned() };
+        let tick = I24::from_be_bytes(tick_bytes);
+
+        let protocol_fee_bytes =
+            unsafe { (data.as_ptr().add(6) as *const [u8; 3]).read_unaligned() };
+        let protocol_fee = U24::from_be_bytes(protocol_fee_bytes);
+
+        let lp_fee_bytes = unsafe { (data.as_ptr().add(3) as *const [u8; 3]).read_unaligned() };
+        let lp_fee = U24::from_be_bytes(lp_fee_bytes);
+
+        Ok((sqrt_price_x96, tick, protocol_fee, lp_fee))
     }
 
     /// Retrieves the tick bitmap of a pool at a specific tick
@@ -117,8 +174,20 @@ where
         // In Solidity:
         // liquidityNet := sar(128, value)
         // liquidityGross := and(value, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
-        let liquidity_gross = u128::from_be_bytes(value.0[16..32].try_into().unwrap());
-        let liquidity_net = i128::from_be_bytes(value.0[0..16].try_into().unwrap());
+        let liquidity_gross = unsafe {
+            // Create a pointer to the start of the second half of the array
+            let gross_ptr = value.as_ptr().add(16) as *const u128;
+            // Read the value in big-endian format
+            u128::from_be(gross_ptr.read_unaligned())
+        };
+
+        let liquidity_net = unsafe {
+            // Create a pointer to the start of the first half of the array
+            let net_ptr = value.as_ptr() as *const i128;
+            // Read the value in big-endian format
+            i128::from_be(net_ptr.read_unaligned())
+        };
+
         Ok((liquidity_gross, liquidity_net))
     }
 }
@@ -142,6 +211,33 @@ mod tests {
             PROVIDER.clone(),
         )
     });
+
+    #[tokio::test]
+    async fn test_get_slot0() {
+        let (sqrt_price_x96_lens, tick_lens, protocol_fee_lens, lp_fee_lens) = POOL_MANAGER
+            .get_slot0(*POOL_ID_ETH_USDC, BLOCK_ID)
+            .await
+            .unwrap();
+
+        let slot0_state_view = STATE_VIEW
+            .getSlot0(*POOL_ID_ETH_USDC)
+            .block(BLOCK_ID.unwrap())
+            .call()
+            .await
+            .unwrap();
+
+        assert_ne!(sqrt_price_x96_lens, U160::ZERO);
+        assert_eq!(
+            sqrt_price_x96_lens, slot0_state_view.sqrtPriceX96,
+            "sqrtPriceX96 mismatch"
+        );
+        assert_eq!(tick_lens, slot0_state_view.tick, "tick mismatch");
+        assert_eq!(
+            protocol_fee_lens, slot0_state_view.protocolFee,
+            "protocolFee mismatch"
+        );
+        assert_eq!(lp_fee_lens, slot0_state_view.lpFee, "lpFee mismatch");
+    }
 
     macro_rules! assert_tick_bitmap_match {
         ($pool_id:expr, $pos:expr, $block_id:expr) => {
@@ -200,9 +296,15 @@ mod tests {
                 .unwrap();
 
             assert_ne!(liquidity_gross_lens, 0);
-            assert_eq!(liquidity_gross_lens, tick_liquidity.liquidityGross);
+            assert_eq!(
+                liquidity_gross_lens, tick_liquidity.liquidityGross,
+                "liquidityGross"
+            );
             assert_ne!(liquidity_net_lens, 0);
-            assert_eq!(liquidity_net_lens, tick_liquidity.liquidityNet);
+            assert_eq!(
+                liquidity_net_lens, tick_liquidity.liquidityNet,
+                "liquidityNet"
+            );
         };
     }
 
